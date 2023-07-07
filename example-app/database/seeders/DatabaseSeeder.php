f3725b9cbf44c510a0eb7e16c30e92214f178156
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-07-07 17:14:12 +0300

    ep28 - Turbo boost with factories

--- example-app/database/seeders/DatabaseSeeder.php
+++ example-app/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,43 +16,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate(); //بيحذف الداتا وبيضيف من جديد
-        Category::truncate();
+//        User::truncate(); //بيحذف الداتا وبيضيف من جديد
+//        Category::truncate();
+//        Post::truncate();
 
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Hatem Khalid'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
 
-        Post::create([
-           'user_id' => $user->id,
-           'category_id' => $family->id,
-           'title' => 'My Family Post',
-           'slug' => 'my-family-post',
-           'excerpt' => 'lorem thie iskd skdflsdf ',
-           'body' => 'This is a body of family post'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'lorem thie iskd skdflsdf ',
-            'body' => 'This is a body of work post'
-        ]);
+//
+//        $personal = Category::create([
+//            'name' => 'Personal',
+//            'slug' => 'personal'
+//        ]);
+//
+//        $family = Category::create([
+//            'name' => 'Family',
+//            'slug' => 'family'
+//        ]);
+//
+//        $work = Category::create([
+//            'name' => 'Work',
+//            'slug' => 'work'
+//        ]);
+//
+//        Post::create([
+//           'user_id' => $user->id,
+//           'category_id' => $family->id,
+//           'title' => 'My Family Post',
+//           'slug' => 'my-family-post',
+//           'excerpt' => 'lorem thie iskd skdflsdf ',
+//           'body' => 'This is a body of family post'
+//        ]);
+//
+//        Post::create([
+//            'user_id' => $user->id,
+//            'category_id' => $work->id,
+//            'title' => 'My Work Post',
+//            'slug' => 'my-work-post',
+//            'excerpt' => 'lorem thie iskd skdflsdf ',
+//            'body' => 'This is a body of work post'
+//        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
