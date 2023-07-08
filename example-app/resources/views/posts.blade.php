38dc9692b443fdfadb8b004c8771de760830478e
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-07-08 15:15:56 +0300

    ep31 - convert html and css to blade

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -1,20 +1,45 @@
 <x-layout>
-    @foreach($posts as $post)
-        <article class="{{$loop->even ? 'even' : 'odd'}}">
-            <h1>
-                <a href="posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <p>
-                <a href="#">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {!!  $post->body  !!}
-            </div>
-        </article>
-    @endforeach
+    @include('_posts-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+             <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
+
 </x-layout>
+
+
+
+
+
+
+
+
+{{--<x-layout>--}}
+{{--    @foreach($posts as $post)--}}
+{{--        <article class="{{$loop->even ? 'even' : 'odd'}}">--}}
+{{--            <h1>--}}
+{{--                <a href="posts/{{$post->slug}}">--}}
+{{--                    {{$post->title}}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
+{{--            <p>--}}
+{{--                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--            </p>--}}
+{{--            <p>--}}
+{{--                <a href="#">{{ $post->category->name }}</a>--}}
+{{--            </p>--}}
+{{--            <div>--}}
+{{--                {!!  $post->body  !!}--}}
+{{--            </div>--}}
+{{--        </article>--}}
+{{--    @endforeach--}}
+{{--</x-layout>--}}
