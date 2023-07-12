67f3ed7aeee8c3f7c7df84a91e9f216cad9b77a1
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-07-12 06:49:26 +0300

    ep44 => simple pagination ♥

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -10,8 +10,16 @@ class PostController extends Controller
     //
     public function index()
     {
+//        return Post::latest()->filter(
+//          request(['search', 'category', 'author'])
+//        )->paginate(6)
+//        ;
+
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(), // solution of N + 1 problem clockwork -  extension بدل ما نعمل جمل سيليكت كثير بنعمل وحدة وممكن تتحقق من خلال ال
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author'])
+            )->paginate(5)->withQueryString(),
+            // solution of N + 1 problem clockwork -  extension بدل ما نعمل جمل سيليكت كثير بنعمل وحدة وممكن تتحقق من خلال ال
         ]);
     }
 
