883845583fb073117a4697e4c8cbeb643b0052d9
Yahyaoy <90440054+Yahyaoy@users.noreply.github.com>
2023-07-19 20:29:27 +0300

    ep68 => Group and store validation logic , تنظيم وتجميع للكونترولر

--- example-app/resources/views/admin/posts/edit.blade.php
+++ example-app/resources/views/admin/posts/edit.blade.php
@@ -1,12 +1,11 @@
 <x-layout>
-
-    <x-setting :heading="'Edit Post: '. $post->title">
+    <x-setting :heading="'Edit Post: ' . $post->title">
         <form method="POST" action="/admin/posts/{{ $post->id }}" enctype="multipart/form-data">
             @csrf
             @method('PATCH')
 
-            <x-form.input name="title" :value="old('title', $post->title)"/>
-            <x-form.input name="slug" :value="old('slug', $post->slug)"/>
+            <x-form.input name="title" :value="old('title', $post->title)" required />
+            <x-form.input name="slug" :value="old('slug', $post->slug)" required />
 
             <div class="flex mt-6">
                 <div class="flex-1">
@@ -16,12 +15,13 @@
                 <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl ml-6" width="100">
             </div>
 
-            <x-form.textarea name="excerpt">{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
-            <x-form.textarea name="body">{{ old('body', $post->body) }}</x-form.textarea>
+            <x-form.textarea name="excerpt" required>{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
+            <x-form.textarea name="body" required>{{ old('body', $post->body) }}</x-form.textarea>
 
             <x-form.field>
                 <x-form.label name="category"/>
-                <select name="category_id" id="category_id">
+
+                <select name="category_id" id="category_id" required>
                     @foreach (\App\Models\Category::all() as $category)
                         <option
                             value="{{ $category->id }}"
@@ -29,9 +29,11 @@
                         >{{ ucwords($category->name) }}</option>
                     @endforeach
                 </select>
+
                 <x-form.error name="category"/>
             </x-form.field>
-            <x-form.button>Publish</x-form.button>
+
+            <x-form.button>Update</x-form.button>
         </form>
     </x-setting>
 </x-layout>
